660e355c70f3bb59255719b59bdfed3431f14408
kisoff <kosh-ev@yandex.ru>
2022-01-11 15:10:38 +0400

    изменено входное задание

--- src/getValue.java
+++ src/getValue.java
@@ -4,9 +4,11 @@ public class getValue {
     public static void main(String[] args) {
         int sumCh = 0;
         int sumNeCh = 0;
+        int firstNumber;
+        int count = 0;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите 15 целых чисел");
-        int[] number = new int[15];
+        System.out.println("Введите 12 целых чисел");
+        int[] number = new int[12];
         for (int i=0 ; i< number.length; i++ ) {
             number[i] = scanner.nextInt();
         }
@@ -15,13 +17,13 @@ public class getValue {
         System.out.print(number[i] + ", " );
         }
 
-        for (int i=0 ; i< number.length; i++ ) {
-            if (number[i]% 2 == 0){
-                sumCh = sumCh + 1 ;
-        } else {
-            sumNeCh = sumNeCh + 1;
+        firstNumber = number[0];
+
+           for (int i=1 ; i< number.length; i++ ) {
+            if (firstNumber < number[i]){
+                count = count + 1 ;
         }
         }
-        System.out.println("Из них четных " + sumCh + ". нечетных " + sumNeCh );
+        System.out.println("Из них чисел, большее чем первое " + count);
     }
     }
